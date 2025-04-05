ea1f5c2e8f52e828e08f9246a9018577206a017d
take-cantik <take.cantik17@gmail.com>
2025-04-05 18:02:40 +0900

    :sparkles: CORSの設定

--- server/main.py
+++ server/main.py
@@ -1,8 +1,25 @@
-from typing import Union
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:3000",
+]
+
+# CORSの設定
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def read_root():
     return {"message": "ok"}
+
+@app.post("/code")
+def create_code():
+    return {"message": "code created!"}
